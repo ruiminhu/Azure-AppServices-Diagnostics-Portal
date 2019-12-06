65b3d5bc20f9529a70125c60a098e004371b6181
Hawk Foreste <hawfor@microsoft.com>
2019-12-06 14:29:31 -0800

    Capture exeptions when hitting server failures

--- ApplensBackend/Configuration/StartupNationalCloud.cs
+++ ApplensBackend/Configuration/StartupNationalCloud.cs
@@ -18,6 +18,8 @@ using Microsoft.IdentityModel.Tokens;
 using Microsoft.IdentityModel.Tokens.Saml2;
 using AppLensV3.Authorization;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Diagnostics;
 
 namespace AppLensV3.Configuration
 {
@@ -109,6 +111,22 @@ namespace AppLensV3.Configuration
 
         public virtual void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseExceptionHandler(errorApp =>
+            {
+                errorApp.Run(async context =>
+                {
+                    context.Response.StatusCode = 500;
+                    context.Response.ContentType = "text/html";
+                    await context.Response.WriteAsync("<html lang=\"en\"><body>\r\n");
+                    await context.Response.WriteAsync("ERROR!<br><br>\r\n");
+                    var exceptionHandlerPathFeature = context.Features.Get<IExceptionHandlerPathFeature>();
+                    await context.Response.WriteAsync(exceptionHandlerPathFeature?.Error?.ToString());
+                    await context.Response.WriteAsync("<a href=\"/\">Home</a><br>\r\n");
+                    await context.Response.WriteAsync("</body></html>\r\n");
+                    await context.Response.WriteAsync(new string(' ', 512));
+                });
+            });
+
             app.UseCors(cors =>
                 cors
                 .AllowAnyHeader()
